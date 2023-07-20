6f724865c3862b9237e201eed9b2c02dd4f93bb2
gyqls234 <gyqls234@naver.com>
2023-07-20 09:50:53 +0900

    springio practice done

---
+++ 00-springstarter/src/main/resources/static/app.js
@@ -0,0 +1,65 @@
+const stompClient = new StompJs.Client({
+    brokerURL: 'ws://localhost:8080/gs-guide-websocket'
+});
+
+stompClient.onConnect = (frame) => {
+    setConnected(true);
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/topic/greetings', (greeting) => {
+        showGreeting(JSON.parse(greeting.body).content);
+    });
+};
+
+stompClient.onWebSocketError = (error) => {
+    console.error('Error with websocket', error);
+};
+
+stompClient.onStompError = (frame) => {
+    console.error('Broker reported error: ' + frame.headers['message']);
+    console.error('Additional details: ' + frame.body);
+};
+
+function setConnected(connected) {
+    const conversationWindow = document.querySelector("#conversation");
+    const connectButton = document.querySelector("#connect");
+    connectButton.disabled = connected;
+
+    const disconnectButton = document.querySelector("#disconnect");
+    disconnectButton.disabled = !connected;
+
+    if (connected) {
+        conversationWindow.style.display = "block";
+    }
+    else {
+        conversationWindow.style.display = "none";
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    stompClient.activate();
+}
+
+function disconnect() {
+    stompClient.deactivate();
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.publish({
+        destination: "/app/hello",
+        body: JSON.stringify({'name': $("#name").val()})
+    });
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', (e) => e.preventDefault());
+    $( "#connect" ).click(() => connect());
+    $( "#disconnect" ).click(() => disconnect());
+    $( "#send" ).click(() => sendName());
+});
